28ae8d3698a209410617a3743d3c556cb671b9ff
RavianXReaver <raveenshenal@gmail.com>
2021-01-31 14:06:49 +0530

    Added config file

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Token: "ODAzOTMxMDM1ODA0NDk5OTc5.YBE9Hw.ON5NFzWKrCyMrzXsVYdWwoSrVjQ",
+    Prefix: ">",
+    Role:"igibuattention",
+    Status:"with Kugel-kun's Computer's",
+    instanceIDS: ['i-0d628c2b3aa73fd72'],
+}
